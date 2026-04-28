5dffb0265e02ba9e7d69ae9e2ea8c3cc091dee21
Nicolas Azambuja <nicolas.azambuja@poatek.com>
2026-04-28 14:47:21 -0300

    created postgres database, with docker and used config lib and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AddressModule } from './address/address.module';
 import { PlaceModule } from './place/place.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AddressModule, PlaceModule],
+  imports: [
+    AddressModule,
+    PlaceModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
   controllers: [],
   providers: [],
 })
