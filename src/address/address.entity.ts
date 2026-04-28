9a33378d66803348fd2884b50cd3b8a9012259a2
Nicolas Azambuja <nicolas.azambuja@poatek.com>
2026-04-28 14:54:28 -0300

    created entity for address and place with typeorm

--- src/address/address.entity.ts
+++ src/address/address.entity.ts
@@ -1,7 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("address")
 export class AddressEntity{
+    
+    @PrimaryGeneratedColumn()
     id!: number;
+
+    @Column({name: "name", length: 100, nullable: false})
     street!: string;
+
+    @Column({ name: "priceRange", length: 15, nullable: false})
     number!: number;
+
+    @Column({name: "name", length: 100, nullable: false})
     neighborhood!: string;
+
+    @Column({ name: "priceRange", length: 15, nullable: false})
     cep!: number;
 }
--- src/place/place.entity.ts
+++ src/place/place.entity.ts
@@ -1,7 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "places"})
 export class PlaceEntity {
+
+    @PrimaryGeneratedColumn()
     id!: number;
+
+    @Column({name: "name", length: 100, nullable: false})
     name!: string;
+
+    @Column({ name: "category", length: 70, nullable: false})
     category!: string;
+
+    @Column({ name: "priceRange", length: 15, nullable: false})
     priceRange!: number;
+
+    @Column({ name: "rating", length: 3, nullable: false})
     rating!: number;
 }
